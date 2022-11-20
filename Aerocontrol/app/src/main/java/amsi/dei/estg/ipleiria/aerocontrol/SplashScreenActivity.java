818270eb5f2e654cbc25a407bd31e0d692eccba4
RafaelBento7 <rafaelcarreirabento7@gmail.com>
2022-11-20 18:21:55 +0000

    splash screen

---
+++ Aerocontrol/app/src/main/java/amsi/dei/estg/ipleiria/aerocontrol/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package amsi.dei.estg.ipleiria.aerocontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        /*Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(SplashScreenActivity.this, SplashScreenActivity.class);
+            startActivity(intent);
+            finish();
+        },3000);*/
+
+        //Retira a Action Bar provisóriamente
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+    }
+}
