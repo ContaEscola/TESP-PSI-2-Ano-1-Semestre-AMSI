ca154eb9225e8bd0f6a2e1992d29bc3f4d3d7c12
RafaelBento7 <rafaelcarreirabento7@gmail.com>
2022-11-29 11:36:32 +0000

    Layout dos restantes pagamentos

--- Aerocontrol/app/src/main/java/amsi/dei/estg/ipleiria/aerocontrol/ui/views/PaymentActivity.java
+++ Aerocontrol/app/src/main/java/amsi/dei/estg/ipleiria/aerocontrol/ui/views/PaymentActivity.java
@@ -1,6 +1,7 @@
 package amsi.dei.estg.ipleiria.aerocontrol.ui.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -11,6 +12,13 @@ public class PaymentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //if (creditcard&&debitcard)
         setContentView(R.layout.activity_payment_card);
+        //else setContentView(R.layout.activity_payment_others);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.Payment_Toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_back);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
