292ca41d696558e36e63c11831e82d6dd0b360a6
pedrohenriques888@live.com <pedrohenriques888@live.com>
2022-12-16 17:19:54 +0000

    Adicionar apiendpoint para as lojas

--- Aerocontrol/app/src/main/java/amsi/dei/estg/ipleiria/aerocontrol/data/network/ApiEndPoint.java
+++ Aerocontrol/app/src/main/java/amsi/dei/estg/ipleiria/aerocontrol/data/network/ApiEndPoint.java
@@ -1,7 +1,7 @@
 package amsi.dei.estg.ipleiria.aerocontrol.data.network;
 
 public class ApiEndPoint {
-    private final static String IP = "http://10.80.227.151:80";
+    private final static String IP = "http://192.168.1.68:80";
     private final static String FOLDER_NAME = "/TESP-PSI-2-Ano-1-Semestre-PLSI-SIS";
     private final static String ENDPOINT = IP + FOLDER_NAME;
     private final static String API_ENDPOINT = ENDPOINT + "/aerocontrol/backend/web/api/";
@@ -12,4 +12,6 @@ public class ApiEndPoint {
     public final static String RESTAURANTS_IMAGE_FOLDER = API_FOLDER_ENDPOINT + "logos/restaurants/";
     public final static String RESTAURANTS_ITEMS_IMAGE_FOLDER = RESTAURANTS_IMAGE_FOLDER + "items/";
 
+    public final static String Stores = API_ENDPOINT + "stores";
+    public final static String STORES_IMAGE_FOLDER = API_FOLDER_ENDPOINT + "logos/stores";
 }
