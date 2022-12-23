4f07a962f425cd23dd18095fe82979be6d442e16
RafaelBento7 <rafaelcarreirabento7@gmail.com>
2022-12-23 19:38:55 +0000

    Endpoints

--- Aerocontrol/app/src/main/java/amsi/dei/estg/ipleiria/aerocontrol/data/network/ApiEndPoint.java
+++ Aerocontrol/app/src/main/java/amsi/dei/estg/ipleiria/aerocontrol/data/network/ApiEndPoint.java
@@ -1,11 +1,13 @@
 package amsi.dei.estg.ipleiria.aerocontrol.data.network;
 
 public class ApiEndPoint {
-    private final static String IP = "http://192.168.1.68:80";
-    private final static String FOLDER_NAME = "/TESP-PSI-2-Ano-1-Semestre-PLSI-SIS";
+    private final static String IP = "http://192.168.1.66:80";
+    private final static String FOLDER_NAME = "/projetofinal";
     private final static String ENDPOINT = IP + FOLDER_NAME;
     private final static String API_ENDPOINT = ENDPOINT + "/aerocontrol/backend/web/api/";
     private final static String API_FOLDER_ENDPOINT = ENDPOINT + "/aerocontrol/uploads/";
+
+    public final static String LOGIN = API_ENDPOINT + "auth/login";
 
     //Entities
     public final static String RESTAURANTS = API_ENDPOINT + "restaurants";
@@ -15,5 +17,6 @@ public class ApiEndPoint {
     public final static String STORES = API_ENDPOINT + "stores";
     public final static String STORES_IMAGE_FOLDER = API_FOLDER_ENDPOINT + "logos/stores/";
 
-    public final static String LOGIN = API_ENDPOINT + "auth/login";
+    public final static String TICKETS = API_ENDPOINT + "flighttickets/";
+    public final static String MY_TICKETS = TICKETS + "mytickets";
 }
