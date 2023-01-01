9070672fbdc5c9784855474349331ca9a4271fff
RafaelBento7 <rafaelcarreirabento7@gmail.com>
2023-01-01 18:16:39 +0000

    Alteracoes e funcao validateUser

--- Aerocontrol/app/src/main/java/amsi/dei/estg/ipleiria/aerocontrol/utils/UserValidations.java
+++ Aerocontrol/app/src/main/java/amsi/dei/estg/ipleiria/aerocontrol/utils/UserValidations.java
@@ -1,25 +1,24 @@
 package amsi.dei.estg.ipleiria.aerocontrol.utils;
 
-import android.content.res.Resources;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import amsi.dei.estg.ipleiria.aerocontrol.R;
+import amsi.dei.estg.ipleiria.aerocontrol.data.db.models.User;
 
 public class UserValidations {
 
-    public static final String usernameError = Resources.getSystem().getString(R.string.username_error);
-    public static final String passwordError = Resources.getSystem().getString(R.string.password_error);
-    public static final String firstNameError = Resources.getSystem().getString(R.string.first_name_error);
-    public static final String lastNameError = Resources.getSystem().getString(R.string.last_name_error);
-    public static final String genderError = Resources.getSystem().getString(R.string.gender_error);
-    public static final String countryError = Resources.getSystem().getString(R.string.country_error);
-    public static final String cityError = Resources.getSystem().getString(R.string.city_error);
-    public static final String emailError = Resources.getSystem().getString(R.string.email_error);
-    public static final String phoneError = Resources.getSystem().getString(R.string.phone_error);
-    public static final String phoneCodeError = Resources.getSystem().getString(R.string.phone_code_error);
-    public static final String birthdateError = Resources.getSystem().getString(R.string.birthdate_error);
+    public static final String usernameError = App.getRes().getString(R.string.username_error);
+    public static final String passwordError = App.getRes().getString(R.string.password_error);
+    public static final String firstNameError = App.getRes().getString(R.string.first_name_error);
+    public static final String lastNameError = App.getRes().getString(R.string.last_name_error);
+    public static final String genderError = App.getRes().getString(R.string.gender_error);
+    public static final String countryError = App.getRes().getString(R.string.country_error);
+    public static final String cityError = App.getRes().getString(R.string.city_error);
+    public static final String emailError = App.getRes().getString(R.string.email_error);
+    public static final String phoneError = App.getRes().getString(R.string.phone_error);
+    public static final String phoneCodeError = App.getRes().getString(R.string.phone_code_error);
+    public static final String birthdateError = App.getRes().getString(R.string.birthdate_error);
 
     public static boolean validateUsername(String username){
         if (!Validations.validateRequired(username)) return false;
@@ -87,16 +86,33 @@ public class UserValidations {
         return Validations.validateDateFormat(birthdate);
     }
 
-    public static HashMap<String,ArrayList<String>> validateUser(){
-        /*HashMap<String, ArrayList<String>> hashMap = new HashMap<>();
+    public static HashMap<String,ArrayList<String>> validateUser(User user){
+        HashMap<String, ArrayList<String>> errors = new HashMap<>();
         ArrayList<String> contacts = new ArrayList<>();
         ArrayList<String> personalData = new ArrayList<>();
         ArrayList<String> accessData = new ArrayList<>();
-        contacts.add("Phone");
-        hashMap.put("Contactos",contacts);
+        // Valida os contactos
+        if (!validateEmail(user.getEmail())) contacts.add("Email");
+        if (!validatePhoneCode(user.getPhoneCountryCode())) contacts.add("Código de telefone do país");
+        if (!validatePhone(user.getPhone())) contacts.add("Telefone");
+        // Valida os dados de acesso
+        if (!validateUsername(user.getUsername())) accessData.add("Username");
+        if (user.getPassword() != null) {
+            if (user.getPassword().length() > 0 || !validatePassword(user.getPassword()))
+                accessData.add("Password");
+        }
+        // Valida os dados pessoais
+        if (!validateFirstName(user.getFirstName())) personalData.add("Primeiro nome");
+        if (!validateLastName(user.getLastName())) personalData.add("Último nome");
+        if (!validateGender(user.getGender())) personalData.add("Género");
+        if (!validateBirthdate(user.getBirthdate())) personalData.add("Data de Nascimento");
+        if (!validateCountry(user.getCountry())) personalData.add("País");
+        if (!validateCity(user.getCity())) personalData.add("Cidade");
 
-        UsernameValid.validateusername(binding.username.gettext);
-        usernamevalid.validUser(usertoupdate);*/
-        return null;
+        if (contacts.size() > 0) errors.put("Contacts", contacts);
+        if (personalData.size() > 0) errors.put("PersonalData", personalData);
+        if (accessData.size() > 0) errors.put("AccessData", accessData);
+
+        return errors.size() > 0 ? errors : null;
     }
 }
