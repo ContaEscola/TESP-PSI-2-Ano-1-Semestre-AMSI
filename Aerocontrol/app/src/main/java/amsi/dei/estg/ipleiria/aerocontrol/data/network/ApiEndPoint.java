b5dbe8136d4e0265d10b81f738bd63fc3bc33e0d
RafaelBento7 <rafaelcarreirabento7@gmail.com>
2023-01-17 15:58:10 +0000

    Fix endpoints

--- Aerocontrol/app/src/main/java/amsi/dei/estg/ipleiria/aerocontrol/data/network/ApiEndPoint.java
+++ Aerocontrol/app/src/main/java/amsi/dei/estg/ipleiria/aerocontrol/data/network/ApiEndPoint.java
@@ -5,18 +5,18 @@ public class ApiEndPoint {
     private final static String FOLDER_NAME = "/projetofinal";
     private final static String ENDPOINT = IP + FOLDER_NAME;
     private final static String API_ENDPOINT = ENDPOINT + "/aerocontrol/backend/web/api/";
-    private final static String API_FOLDER_ENDPOINT = ENDPOINT + "/aerocontrol/uploads/";
+    private final static String API_FOLDER_ENDPOINT = ENDPOINT + "/aerocontrol/common/uploads/";
 
     public final static String LOGIN = API_ENDPOINT + "auth/login";
     public final static String UPDATE_ACCOUNT = API_ENDPOINT + "user/";
 
     //Entities
     public final static String RESTAURANTS = API_ENDPOINT + "restaurants";
-    public final static String RESTAURANTS_IMAGE_FOLDER = API_FOLDER_ENDPOINT + "logos/restaurants/";
+    public final static String RESTAURANTS_IMAGE_FOLDER = API_FOLDER_ENDPOINT + "restaurants/";
     public final static String RESTAURANTS_ITEMS_IMAGE_FOLDER = RESTAURANTS_IMAGE_FOLDER + "items/";
 
     public final static String STORES = API_ENDPOINT + "stores";
-    public final static String STORES_IMAGE_FOLDER = API_FOLDER_ENDPOINT + "logos/stores/";
+    public final static String STORES_IMAGE_FOLDER = API_FOLDER_ENDPOINT + "stores/";
 
     public final static String TICKETS = API_ENDPOINT + "flighttickets/";
     public final static String MY_TICKETS = TICKETS + "mytickets";
