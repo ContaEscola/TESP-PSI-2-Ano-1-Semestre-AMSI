d8ceb7ec353528e730e7543af2b4142813355736
RafaelBento7 <rafaelcarreirabento7@gmail.com>
2023-01-20 17:44:23 +0000

    DatePickers e alteração butoes no searchFlight

--- Aerocontrol/app/src/main/java/amsi/dei/estg/ipleiria/aerocontrol/ui/views/FlightSearchFragment.java
+++ Aerocontrol/app/src/main/java/amsi/dei/estg/ipleiria/aerocontrol/ui/views/FlightSearchFragment.java
@@ -1,14 +1,20 @@
 package amsi.dei.estg.ipleiria.aerocontrol.ui.views;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import amsi.dei.estg.ipleiria.aerocontrol.R;
 import amsi.dei.estg.ipleiria.aerocontrol.data.db.models.Airport;
@@ -19,6 +25,10 @@ import amsi.dei.estg.ipleiria.aerocontrol.listeners.AirportsListener;
 public class FlightSearchFragment extends Fragment implements AirportsListener {
 
     FragmentFlightSearchBinding binding;
+
+    private boolean two_way_trip = true;
+
+    private Calendar calendar;
 
     public FlightSearchFragment() {
         // Required empty public constructor
@@ -33,7 +43,68 @@ public class FlightSearchFragment extends Fragment implements AirportsListener {
         SingletonFlights.getInstance(getContext()).setAirportsListener(this);
         SingletonFlights.getInstance(getContext()).getAirportsAPI(getContext());
 
+        binding.FlightSearchBtOneWay.setOnClickListener(v -> {
+            if (!two_way_trip) return;
+            two_way_trip = false;
+            binding.FlightSearchBtRoundTrip.setTextColor(getResources().getColor(R.color.blue_400));
+            binding.FlightSearchBtOneWay.setTextColor(getResources().getColor(R.color.white));
+            binding.FlightSearchBtOneWay.setBackgroundResource(R.drawable.button_pill_primary);
+            binding.FlightSearchBtRoundTrip.setBackgroundResource(R.drawable.button_pill_primary_outline_background_states);
+        });
+
+        binding.FlightSearchBtRoundTrip.setOnClickListener(v -> {
+            if (two_way_trip) return;
+            two_way_trip = true;
+            binding.FlightSearchBtRoundTrip.setTextColor(getResources().getColor(R.color.white));
+            binding.FlightSearchBtOneWay.setTextColor(getResources().getColor(R.color.blue_400));
+            binding.FlightSearchBtRoundTrip.setBackgroundResource(R.drawable.button_pill_primary);
+            binding.FlightSearchBtOneWay.setBackgroundResource(R.drawable.button_pill_primary_outline_background_states);
+        });
+
+        startCalender();
+        binding.FlightSearchEtDepartureDate.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) showDatePicker(true);
+        });
+        binding.FlightSearchEtArrivalDate.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) showDatePicker(false);
+        });
+
         return view;
+    }
+
+    private void startCalender(){
+        Date date = new Date();
+        calendar = new GregorianCalendar();
+        calendar.setTime(date);
+    }
+
+    /**
+     * Mostra o datePicker
+     * @param departureDate True se o user clicar na data de origem, false se clicar na data de chegada
+     */
+    private void showDatePicker(final boolean departureDate){
+        DatePickerDialog picker = new DatePickerDialog(getContext(), R.style.customDatePickerStyle,
+                (view, year, monthOfYear, dayOfMonth) -> {
+                    String date_dd_mm_yyyy = getString(R.string.date_format,dayOfMonth,monthOfYear+1,year);
+                    if (departureDate)
+                        binding.FlightSearchEtDepartureDate.setText(date_dd_mm_yyyy);
+                    else binding.FlightSearchEtArrivalDate.setText(date_dd_mm_yyyy);
+                }, calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH));
+
+        picker.setTitle(getString(R.string.insert_birth_date));
+
+        picker.setOnShowListener(dialog -> {
+            Button postiveBt = picker.getButton(DialogInterface.BUTTON_POSITIVE);
+            Button negativeBt = picker.getButton(DialogInterface.BUTTON_NEGATIVE);
+            postiveBt.setTextColor(getResources().getColor(R.color.black_400));
+            postiveBt.setText(R.string.confirm);
+            negativeBt.setTextColor(getResources().getColor(R.color.black_400));
+            negativeBt.setText(R.string.cancel);
+        });
+
+        picker.show();
     }
 
     @Override
@@ -41,8 +112,5 @@ public class FlightSearchFragment extends Fragment implements AirportsListener {
         ArrayAdapter<Airport> adapter = new ArrayAdapter<>(getContext(), R.layout.gender_list_item, airports);
         binding.FlightSearchACTVOrigin.setAdapter(adapter);
         binding.FlightSearchACTVDestiny.setAdapter(adapter);
-        binding.FlightSearchACTVOrigin.setOnItemClickListener((parent, view, position, id) -> {
-
-        });
     }
 }
