5da9c43a641c4fe03ad172781dcd304c483a5567
ContaEscola <2211904@my.ipleiria.pt>
2023-01-21 21:57:14 +0000

    Fix nos names na ApiEndPoint, adicionei um ApiConfig

---
+++ Aerocontrol/app/src/main/java/amsi/dei/estg/ipleiria/aerocontrol/data/network/ApiConfig.java
@@ -0,0 +1,7 @@
+package amsi.dei.estg.ipleiria.aerocontrol.data.network;
+
+public class ApiConfig {
+
+    // Existem alguns atributos na api que devolvem N/A como null
+    public static final String API_CUSTOM_NULL = "N/A";
+}
--- Aerocontrol/app/src/main/java/amsi/dei/estg/ipleiria/aerocontrol/data/network/ApiEndPoint.java
+++ Aerocontrol/app/src/main/java/amsi/dei/estg/ipleiria/aerocontrol/data/network/ApiEndPoint.java
@@ -1,15 +1,15 @@
 package amsi.dei.estg.ipleiria.aerocontrol.data.network;
 
 public class ApiEndPoint {
-    private final static String IP = "http://10.80.227.151:80";
-    private final static String FOLDER_NAME = "/TESP-PSI-2-Ano-1-Semestre-PLSI-SIS";
-    private final static String ENDPOINT = IP + FOLDER_NAME;
-    private final static String API_ENDPOINT = ENDPOINT + "/aerocontrol/backend/web/api/";
-    private final static String API_FOLDER_ENDPOINT = ENDPOINT + "/aerocontrol/uploads/";
+    private final static String IP = "http://192.168.8.100:80";
+
+    private final static String API_ROOT_FOLDER = IP + "/TESP-PSI-2-Ano-1-Semestre-PLSI-SIS/aerocontrol";
+    private final static String ENDPOINT_API = API_ROOT_FOLDER + "/backend/web/api";
+    private final static String ENDPOINT_API_UPLOADS = API_ROOT_FOLDER + "/common/uploads";
 
     //Entities
-    public final static String RESTAURANTS = API_ENDPOINT + "restaurants";
-    public final static String RESTAURANTS_IMAGE_FOLDER = API_FOLDER_ENDPOINT + "logos/restaurants/";
-    public final static String RESTAURANTS_ITEMS_IMAGE_FOLDER = RESTAURANTS_IMAGE_FOLDER + "items/";
+    public final static String ENDPOINT_RESTAURANTS = ENDPOINT_API + "/restaurants";
+    public final static String ENDPOINT_RESTAURANTS_UPLOADS = ENDPOINT_API_UPLOADS + "/restaurants";
+    public final static String RESTAURANT_ITEMS_FOLDER = "/menu";
 
 }
