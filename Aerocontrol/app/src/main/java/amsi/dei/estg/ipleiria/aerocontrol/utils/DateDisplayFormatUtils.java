94dfd9faa018a23a3344bb947f5424cdea90e05d
ContaEscola <2211904@my.ipleiria.pt>
2023-01-22 13:28:14 +0000

    Adicionei um dateUtil

--- Aerocontrol/app/src/main/java/amsi/dei/estg/ipleiria/aerocontrol/utils/DateDisplayFormatUtils.java
+++ Aerocontrol/app/src/main/java/amsi/dei/estg/ipleiria/aerocontrol/utils/DateDisplayFormatUtils.java
@@ -1,2 +1,23 @@
-package amsi.dei.estg.ipleiria.aerocontrol.utils;public class DateDisplayFormatUtils {
+package amsi.dei.estg.ipleiria.aerocontrol.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateDisplayFormatUtils {
+
+    public static final String DATE_TIME_FORMAT = "dd-MM-yyyy HH:mm";
+    public static final String DATE_FORMAT = "dd-MM-yyyy";
+    public static final String TIME_FORMAT = "HH:mm";
+
+    public static Date formatStringToDate(String stringToFormat) {
+        Date formatedDate = null;
+        try {
+            formatedDate = new SimpleDateFormat(DATE_FORMAT).parse(stringToFormat);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return formatedDate;
+
+    }
 }
